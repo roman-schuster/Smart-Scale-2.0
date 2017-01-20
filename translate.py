7fc7fa5a8cfa9513ad8d326a3656c9cbed74fca3
Roman Schuster <schuster@sas.upenn.edu>
2017-01-20 11:46:58 -0500

    Starting to build the json request

--- translate.py
+++ translate.py
@@ -1,15 +1,12 @@
-def get_token():
-    '''
-    Returns token for Microsoft Azure Marketplace Oauth
-    Token is good for 10 minutes
-    '''
-    
-    url_args = {
-        'client_id': 'Smart-Scale-1',
-        'client_secret': 'b50eae37-5263-4693-81d6-9d7c04bc8872',
-        'scope': 'http://api.microsofttranslator.com',
-        'grant_type': 'client_credentials'
-    }
-    
-    oauth_url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken'
-    oauth_key = '7d2c5a67c24c45ce90fc219a3fae8bf9'
+token = 'test'
+# gotten from command line arg - token will be a base 64 encoded plain-text file
+
+txt = 'here, have some text'
+to_lang = 'fr'
+
+translation_request = {
+    'appid' : ('Bearer ' + token),
+    'text' : txt,
+    'from' : 'en',
+    'to' : to_lang,
+}
