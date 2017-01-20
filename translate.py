52226515031430b663f3ce252d09094a9e366bec
Roman Schuster <schuster@sas.upenn.edu>
2017-01-20 11:31:25 -0500

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,4 +1,15 @@
 def get_token():
     '''
     Returns token for Microsoft Azure Marketplace Oauth
+    Token is good for 10 minutes
     '''
+    
+    url_args = {
+        'client_id': 'Smart-Scale-1',
+        'client_secret': 'b50eae37-5263-4693-81d6-9d7c04bc8872',
+        'scope': 'http://api.microsofttranslator.com',
+        'grant_type': 'client_credentials'
+    }
+    
+    oauth_url = 'https://api.cognitive.microsoft.com/sts/v1.0/issueToken'
+    oauth_key = '7d2c5a67c24c45ce90fc219a3fae8bf9'
